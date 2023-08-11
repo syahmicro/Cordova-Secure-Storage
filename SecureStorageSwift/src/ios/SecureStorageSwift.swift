01a315ff01e5dba339bb66e73c7b715cfdf817ae
Mohd Daud Muhamad Syahrul Fahmie (IFMY CSC FI ADI DES) <MuhamadSyahrulFahmie.MohdDaud@infineon.com>
2023-08-11 17:44:53 +0800

    alert

--- SecureStorageSwift/src/ios/SecureStorageSwift.swift
+++ SecureStorageSwift/src/ios/SecureStorageSwift.swift
@@ -1,7 +1,8 @@
 import Foundation
 import OSCommonPluginLib
 
-@objc(SecureStorageSwift) class SecureStorageSwift : CDVPlugin {
+@objc(SecureStorageSwift) 
+class SecureStorageSwift : CDVPlugin {
 
   @objc(echo:)
   func echo(command: CDVInvokedUrlCommand) {
@@ -11,46 +12,52 @@ import OSCommonPluginLib
 
     let msg = command.arguments[0] as? String ?? ""
 
-    if msg.characters.count > 0 {
-      /* UIAlertController is iOS 8 or newer only. */
-      let toastController: UIAlertController =
-        UIAlertController(
-          title: "",
-          message: msg,
-          preferredStyle: .Alert
-        )
+    let alert = UIAlertView()
+    alert.title = "Alert"
+    alert.message = msg
+    alert.addButtonWithTitle("Understood")
+    alert.show()
+    // if msg.characters.count > 0 {
+    //   /* UIAlertController is iOS 8 or newer only. */
+    //   let toastController: UIAlertController =
+    //     UIAlertController(
+    //       title: "",
+    //       message: msg,
+    //       preferredStyle: .Alert
+    //     )
   
-      self.viewController?.presentViewController(
-        toastController,
-        animated: true,
-        completion: nil
-      )
+    //   self.viewController?.presentViewController(
+    //     toastController,
+    //     animated: true,
+    //     completion: nil
+    //   )
 
-      let duration = Double(NSEC_PER_SEC) * 3.0
+    //   let duration = Double(NSEC_PER_SEC) * 3.0
 
-      dispatch_after(
-        dispatch_time(
-          DISPATCH_TIME_NOW,
-          Int64(duration)
-        ),
-        dispatch_get_main_queue(),
-        {
-          toastController.dismissViewControllerAnimated(
-            true,
-            completion: nil
-          )
-        }
-      )
+    //   dispatch_after(
+    //     dispatch_time(
+    //       DISPATCH_TIME_NOW,
+    //       Int64(duration)
+    //     ),
+    //     dispatch_get_main_queue(),
+    //     {
+    //       toastController.dismissViewControllerAnimated(
+    //         true,
+    //         completion: nil
+    //       )
+    //     }
+    //   )
 
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAsString: msg
-      )
-    }
+    //   pluginResult = CDVPluginResult(
+    //     status: CDVCommandStatus_OK,
+    //     messageAsString: msg
+    //   )
+    // }
 
-    self.commandDelegate!.sendPluginResult(
-      pluginResult,
-      callbackId: command.callbackId
-    )
+    // self.commandDelegate!.sendPluginResult(
+    //   pluginResult,
+    //   callbackId: command.callbackId
+    // )
+
   }
 }
