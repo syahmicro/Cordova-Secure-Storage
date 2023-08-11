a25b162b5cde144fac788ee00d5fe0a321df1c6b
Mohd Daud Muhamad Syahrul Fahmie (IFMY CSC FI ADI DES) <MuhamadSyahrulFahmie.MohdDaud@infineon.com>
2023-08-11 18:12:40 +0800

    update function

--- SecureStorageSwift/src/ios/SecureStorageSwift.swift
+++ SecureStorageSwift/src/ios/SecureStorageSwift.swift
@@ -6,17 +6,28 @@ class SecureStorageSwift : CDVPlugin {
 
   @objc(echo:)
   func echo(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
+    // var pluginResult = CDVPluginResult(
+    //   status: CDVCommandStatus_ERROR
+    // )
 
-    let msg = command.arguments[0] as? String ?? ""
+     self.callbackId = command.callbackId
+        
+        guard
+            let msg = command.arguments[0] as? String ?? ""
+        else {
+            self.sendResult(result: "", error:FirebaseMessagingErrors.subscriptionError as NSError, callBackID: self.callbackId)
+            return
+    }
 
+    Task {
     let alert = UIAlertView()
     alert.title = "Alert"
     alert.message = msg
     alert.addButtonWithTitle("Understood")
     alert.show()
+    }
+    
+
     // if msg.characters.count > 0 {
     //   /* UIAlertController is iOS 8 or newer only. */
     //   let toastController: UIAlertController =
